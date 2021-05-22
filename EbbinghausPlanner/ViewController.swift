d0a09a829ffa45b151634fcabecdc2f10c7f02e2
Avocado34 <avocado34.131@gmail.com>
2021-05-22 22:09:56 +0900

    Initial Commit

---
+++ EbbinghausPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EbbinghausPlanner
+//
+//  Created by 이승기 on 2021/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
